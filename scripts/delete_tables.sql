91bc47ee2ab4e53baf7d3454439e3098f05d0a42
fara0000 <fi@livereach.com>
2022-01-21 21:45:03 +0300

    added sql scripts

---
+++ scripts/delete_tables.sql
@@ -0,0 +1,23 @@
+drop table if exists book;
+
+drop table if exists library;
+
+drop table if exists meeting;
+
+drop table if exists synagogue_attribute;
+
+drop table if exists premise;
+
+drop table if exists tradition_attribute;
+
+drop table if exists attribute;
+
+drop table if exists event_member;
+
+drop table if exists member;
+
+drop table if exists event;
+
+drop table if exists synagogue;
+
+drop table if exists tradition;
---
+++ scripts/triggers_and_functions.sql
@@ -0,0 +1,55 @@
+create or replace function check_max_visitors_function() returns trigger as $check_max_visitors_function$
+declare
+    max_visitors           integer;
+    current_visitors_count integer;
+BEGIN
+    SELECT count(*)
+    INTO current_visitors_count
+    FROM event_member
+    WHERE event_member.event_id = NEW.event_id;
+    SELECT meeting.max_visitors INTO max_visitors FROM meeting
+    WHERE event = NEW.event_id;
+    IF (current_visitors_count = max_visitors) THEN
+        RAISE EXCEPTION 'Visitors are limited, try next time!';
+    END IF;
+
+    return NEW;
+END;
+
+$check_max_visitors_function$ LANGUAGE plpgsql;
+
+
+create trigger check_max_visitors
+    before INSERT on event_member
+    for each row execute procedure check_max_visitors_function();
+
+create function book_was_borrowed_function() returns trigger as $book_was_borrowed_function$
+BEGIN
+    IF borrower is null THEN
+        UPDATE book
+        SET is_available = true
+        WHERE book_id = NEW.book_id;
+    ELSE
+        UPDATE book
+        SET is_available = false
+        WHERE book_id = NEW.book_id;
+    END IF;
+    RETURN new;
+END;
+
+$book_was_borrowed_function$ LANGUAGE plpgsql;
+
+create trigger cell_was_updated
+    after UPDATE of borrower on book
+    for each row execute procedure book_was_borrowed_function();
+
+
+create function update_role(in our_member_id integer, in new_role varchar(50)) returns text as $$
+BEGIN
+    update member
+    set role = new_role
+    where member_id = our_member_id;
+    return 'New role successfully updated!';
+END;
+
+$$ LANGUAGE plpgsql;
